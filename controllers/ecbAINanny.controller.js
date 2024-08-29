b68744e50bbb106518bb879f6edfe80c87499ef0
Chanuka Abeysinghe <abeysinghechanuka@gmail.com>
2024-08-29 06:40:04 +0530

    Update ecbAINanny.controller.js

--- controllers/ecbAINanny.controller.js
+++ controllers/ecbAINanny.controller.js
@@ -170,8 +170,7 @@ export const addNewQuestion = async (req, res) => {
 
     // Make a request to the external API
     const response = await axios.post(
-      "https://smartbabyai2-dfgfeeevb6f6e2c6.eastus-01.azurewebsites.net/ask,
-      // "https://smartbabyai2-dfgfeeevb6f6e2c6.eastus-01.azurewebsites.net/ask",
+      "https://smartbabyai2-dfgfeeevb6f6e2c6.eastus-01.azurewebsites.net/ask",
       requestData
     );
 
